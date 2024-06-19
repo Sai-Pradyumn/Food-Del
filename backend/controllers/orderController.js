b18b703f10fc83750ffdfd17721740e845ba7902
Sai-Pradyumn <saiprady147@gmail.com>
2024-06-19 19:04:33 +0530

    mod10

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -9,7 +9,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing user order for frontend
 const placeOrder = async (req,res) => {
 
-    const frontend_url = "http://localhost:5174";
+    // const frontend_url = "http://localhost:5174";
+    const frontend_url = "https://food-del-frontend-r977.onrender.com";
 
     try{
         const newOrder = new orderModel({
